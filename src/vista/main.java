4f3e11c731c59ab2af954674d6053a23c22274fb
tephyMoon <tephy.andrinov@gmail.com>
2024-08-02 19:15:16 -0600

    Primer commit

---
+++ src/vista/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Maritza Vargas
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora frm = new Calculadora();
+        frm.show();
+    }
+}
